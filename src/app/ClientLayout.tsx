dbdcae99128227262af6b69f4e49992b150d1cfd
I don´t know/ <gelsonganga668@gmail.com>
2025-11-02 14:56:41 +0100

    grt

--- src/app/ClientLayout.tsx
+++ src/app/ClientLayout.tsx
@@ -1,12 +1,9 @@
 "use client";
 
-import { AuthProvider } from "@/context/AuthContext";
-
 export default function ClientLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return <AuthProvider>{children}</AuthProvider>;
+  return <>{children}</>;
 }
-
